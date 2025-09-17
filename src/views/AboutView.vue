1b3690d860a47e90f96cfb8d907d43190f0deb62
CptFranck <francois.caillet71@gmail.com>
2025-09-17 16:49:31 +0200

    git update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import RefineryComponent from '@/components/Refinery.vue'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <RefineryComponent/>
+<!--  <div class="about">-->
+<!--    <h1>This is an about page</h1>-->
+<!--  </div>-->
 </template>
 
 <style>
