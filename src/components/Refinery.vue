aff43ae64a9f15711a9c852fa075db4a036448d1
CptFranck <francois.caillet71@gmail.com>
2025-09-19 17:04:58 +0200

    update refinery component

--- src/components/Refinery.vue
+++ src/components/Refinery.vue
@@ -1,91 +1,73 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+  import { Recipe } from '@/class/Recipe.ts'
+  import { Resource } from '@/class/Resource.ts'
+
   defineOptions({
     name: 'RefineryComponent'
   });
 
-  // const rawResources = {
-  //   aluminum: 0,
-  //   coal: 0,
-  //   component: 0,
-  //   copper: 0,
-  //   damageComponent: 0,
-  //   iron: 0,
-  //   rareMaterial: 0,
-  //   salvage: 0,
-  //   sulfur: 0,
-  //   wreckage: 0,
-  // };
-  //
-  // const material = {
-  //   basicMaterial: 0,
-  //   coke: 0,
-  //   constructionMaterial: 0,
-  //   explosivePowder: 0,
-  //   gravel: 0,
-  //   heavyExplosivePowder: 0,
-  //   refinedMaterials: 0,
-  // };
-  //
-  // const technologyMaterials = {
-  //   aluminiumAlloy: 0,
-  //   copperAlloy: 0,
-  //   ironAlloy: 0,
-  // };
-  //
-  // const liquids = {
-  //   diesel: 0,
-  //   enrichedOil: 0,
-  //   heavyOil: 0,
-  //   oil: 0,
-  //   petrol: 0,
-  //   water: 0,
-  // };
 
+  const refiningOption : Array<Recipe> = [
+    new Recipe({ salvage: { resource: new Resource("Salvage", 0), ratio: 2 }},
+      { basicMaterial: { resource: new Resource("Basic Material", 0), ratio: 1 }}),
+    new Recipe({ salvage: { resource: new Resource("Salvage", 0), ratio: 5 }},
+      { diesel: { resource: new Resource("Diesel", 0), ratio: 1 }})
+  ];
+    // basicMaterial: {
+    //   inputs: { resource : salvage,  value: 0 },
+    //   outputs: [{ name: "Basic Material", type: "basicMaterial", value: 0 }],
+    //   // time: { h: 0, min: 0, sec: 0, ms: 48 },
+    //   personalOutputLimit: 12500,
+    //   publicStockpileLimit: 32000,
+    // },
+    // diesel: {
+    //   inputs: { salvage: 5 },
+    //   outputs: { gas: 1 },
+    //   time: { h: 0, min: 0, sec: 12, ms: 0 },
+    //   personalOutputLimit: 6000,
+    //   publicStockpileLimit: 32000,
+    // },
+  // };
+  // const orderPrivacy = ref(false);
 
-  const refiningOption = {
-    basicMaterial: {
-      inputs: { salvage: 2 },
-      outputs: { basicMaterial: 1 },
-      time: { h: 0, min: 0, sec: 0, ms: 48 },
-      personalOutputLimit: 12500,
-      publicStockpileLimit: 32000,
-    },
-    diesel: {
-      inputs: { salvage: 5 },
-      outputs: { gas: 1 },
-      time: { h: 0, min: 0, sec: 12, ms: 0 },
-      personalOutputLimit: 6000,
-      publicStockpileLimit: 32000,
-    },
-  };
-  const orderPrivacy = false;
 </script>
 <template>
   <div>
-    <table border="1" cellpadding="8" cellspacing="0">
+<!--    {{ Object.values(refiningOption).map(ro => ro.inputs.resource).map(i => i.resource.name + ":" + i.resource.quantity).join(', ') }}-->
+<!--    {{ Object.values(refiningOption).map(ro => ro.outputs.resource).map(o => o.resource.name + ":" + o.resource.quantity).join(', ') }}-->
+<!--        {{ Object.values(refiningOption).map(ro => ro.inputs[0].resource).map(r => r.name ).join(', ') }}-->
+    {{  Object.values(refiningOption).map(ro => "r") }}
+    <table>
       <thead>
       <tr>
         <th>Ressource</th>
-        <th>Needed</th>
         <th>Ratio</th>
         <th>Produced</th>
-        <th>Ressource</th>
         <th>Output limit</th>
-<!--        <th>Time</th>-->
+        <th>Time</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(recipe, key) in refiningOption" :key="key">
-        <td><input></td>
-        <td v-for="(value, input) in recipe.inputs" :key="input">{{input}}</td>
+      <tr v-for="recipe in refiningOption" >
         <td>
-          <p v-for="(value, input) in recipe.inputs" :key="input">{{value}} {{input}}</p>
-          ->
-          <p v-for="(value, output) in recipe.outputs" :key="output">{{value}} {{output}}</p>
+          <p v-for="input in recipe.inputs">
+            {{input.resource.name}}
+            <input type="number" v-model.number="input.resource.quantity">
+          </p>
         </td>
-        <td v-for="(value, output) in recipe.outputs" :key="output">{{output}}</td>
-        <td><input></td>
-        <td>/{{ orderPrivacy ? recipe.personalOutputLimit : recipe.publicStockpileLimit }}</td>
+        <td>
+          {{ Object.values(recipe.inputs).map(i => i.ratio + ' ' + i.resource.name).join(', ') }}
+          →
+          {{ Object.values(recipe.outputs).map(o => o.ratio + ' ' + o.resource.name).join(', ') }}
+        </td>
+        <td>
+          <p v-for="output in recipe.outputs">
+            {{output.resource.name}}
+            <input type="number" v-model.number="output.resource.quantity">
+          </p>
+        </td>
+<!--        <td>/{{ orderPrivacy ? recipe.personalOutputLimit : recipe.publicStockpileLimit }}</td>-->
 <!--        <td>{{ new Date(recipe.time)}}</td>-->
       </tr>
       </tbody>
@@ -103,3 +85,5 @@ th, td {
   text-align: center;
 }
 </style>
+
+
