9cdbf752565ce23f84341afc8ae2ca81018a2e99
CptFranck <francois.caillet71@gmail.com>
2025-09-19 17:03:16 +0200

    add recipe class

---
+++ src/class/Recipe.ts
@@ -0,0 +1,46 @@
+import { Resource } from '@/class/Ressource.ts'
+
+interface RecipeEntry {
+  resource: Resource;
+  ratio: number;
+}
+
+export class Recipe {
+  inputs: Record<string, RecipeEntry>;
+  outputs: Record<string, RecipeEntry>;
+  // time : ;
+  // personalOutputLimit: number;
+  // publicStockpileLimit: number;
+
+  constructor(
+    inputs: Record<string, RecipeEntry>,
+    outputs: Record<string, RecipeEntry>
+  ) {
+    this.inputs = inputs;
+    this.outputs = outputs;
+  }
+
+  refine() {
+    const cycles = this.computeMaximumCycle();
+
+    for (const key in this.outputs) {
+      const entry = this.outputs[key];
+      entry.resource.quantity += entry.ratio * cycles;
+    }
+
+    for (const key in this.inputs) {
+      const entry = this.inputs[key];
+      entry.resource.quantity -= entry.ratio * cycles;
+    }
+  }
+
+  private computeMaximumCycle() : number{
+    let cycles = Infinity;
+    for (const key in this.inputs) {
+      const entry = this.inputs[key];
+      const possibleCycles = entry.resource.quantity / entry.ratio;
+      if (possibleCycles < cycles) cycles = possibleCycles;
+    }
+    return cycles;
+  }
+}
