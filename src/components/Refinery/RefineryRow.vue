a36dbb297f0cf9708092bfe365cbd463d2493eb0
CptFranck <francois.caillet71@gmail.com>
2025-09-26 17:12:12 +0200

    add RefineryRow Component

---
+++ src/components/Refinery/RefineryRow.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { formatDuration, Recipe } from '@/class/Recipe.ts'
+
+  defineProps<{
+    recipe: Recipe
+  }>()
+
+</script>
+
+<template>
+  <tr>
+    <td>
+      <span v-for="(input, ki) in recipe.inputs" :key="ki" class="cell-item">
+        <span class="cell-label">{{ input.resource.name }}</span>
+        <input type="number"
+               v-model.number="input.resource.quantity"
+               @input="recipe.updateOutputValue()"
+               min="0"
+        >
+      </span>
+    </td>
+    <td>
+      {{ Object.values(recipe.inputs).map(i => i.ratio).join(' + ') }}
+      →
+      {{ Object.values(recipe.outputs).map(o => o.ratio).join(' + ') }}
+    </td>
+    <td>
+          <span v-for="(output, ko) in recipe.outputs" :key="ko" class="cell-item" >
+            <span class="cell-label">{{ output.resource.name }}</span>
+            <input type="number"
+                   v-model.number="output.resource.quantity"
+                   @input="recipe.updateInputValue()"
+                   min="0"
+            >
+          </span>
+    </td>
+    <td>/...</td>
+    <td>{{ formatDuration(recipe.processTime)}}</td>
+  </tr>
+</template>
+
+<style scoped>
+  td {
+    padding: 8px;
+  }
+  .cell-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .cell-label {
+    flex: 1;
+    text-align: left;
+  }
+</style>
